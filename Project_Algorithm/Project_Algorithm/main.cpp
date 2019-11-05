24aaf164dd14cec213eee909cbd71640b69011e3
Maxime <maxime.langlet@ulb.ac.be>
2019-11-05 18:34:26 +0100

    Essais lecture fichier Fasta

--- Project_Algorithm/Project_Algorithm/main.cpp
+++ Project_Algorithm/Project_Algorithm/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    /*
+    if(argc<=1){
+        cout << "Argument demandé :" << argv[0]<<" [fichier] "<<endl;
+        return 1;
+    }
+    */
+    
+    //ifstream input(argv[1]);
+    ifstream input("/Users/langletmaxime/Desktop/P4/C++/swipe/P00533.fasta");
+    if(input){
+        /*
+        Style de format FASTA :
+        >Rosetta_Example_1
+        THERECANBENOSPACE
+        >Rosetta_Example_2
+        THERECANBESEVERAL
+        LINESBUTTHEYALLMUST
+        BECONCATENATED
+        */
+        string ligne;
+        while(getline(input, ligne)){
+            cout<<ligne<<endl;
+        }
+    }
+    else{
+        cout << "Erreur dans l'ouverture du fichier"<<endl;
+        return 2;
+    }
     return 0;
 }
