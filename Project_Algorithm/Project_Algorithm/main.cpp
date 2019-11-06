583a7bb1d723caf4a45b2293dce23966a55339f5
Maxime <maxime.langlet@ulb.ac.be>
2019-11-06 12:08:06 +0100

    Essais lecture fichier Fasta

--- Project_Algorithm/Project_Algorithm/main.cpp
+++ Project_Algorithm/Project_Algorithm/main.cpp
@@ -24,16 +24,42 @@ int main(int argc, const char * argv[]) {
     if(input){
         /*
         Style de format FASTA :
-        >Rosetta_Example_1
-        THERECANBENOSPACE
-        >Rosetta_Example_2
-        THERECANBESEVERAL
-        LINESBUTTHEYALLMUST
-        BECONCATENATED
+         >Rosetta_Example_1
+         THERECANBENOSPACE
+         >Rosetta_Example_2
+         THERECANBESEVERAL
+         LINESBUTTHEYALLMUST
+         BECONCATENATED
+        Qui donnera au final :
+         Rosetta_Example_1 : THERECANBENOSPACE
+         Rosetta_Example_2 : THERECANBESEVERALLINESBUTTHEYALLMUSTBECONCATENATED
+         
+         En gros ce que fait le code ci-dessous, c'est qu'il regarde prend le nom et ajoute les lignes de manières consécutives plutot que 80 caracteres par 80 caracteres, ce qui est mieux je crois. Ensuite il print le nom et le contenu.
         */
-        string ligne;
+        string ligne, nom, contenu;
         while(getline(input, ligne)){
-            cout<<ligne<<endl;
+            if(ligne.empty() || ligne[0]=='>'){
+                if(!nom.empty()){
+                    cout << nom << ":" << contenu << endl;
+                    nom.clear();
+                }
+                if(!ligne.empty()){
+                    nom=ligne.substr(1); //On enleve le ">" et on retient le nom
+                }
+                contenu.clear();
+            }
+            else if(!nom.empty()){
+                if(ligne.find(' ') != string::npos){
+                    contenu.clear();
+                    nom.clear();
+                }
+                else{
+                    contenu += ligne;
+                }
+            }
+        }
+        if(!nom.empty()){
+            cout << nom << ":" << contenu << endl;
         }
     }
     else{
